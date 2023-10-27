ff508ac90ada45eadcfa72ba6350eda3285335d0
Songpr <songpr@gmail.com>
2023-10-28 00:46:48 +0700

    support async jobHanldle and more test

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 function isNumber(value) {
   return typeof value === "number" && isFinite(value);
 }
+import { types } from "node:util";
+
+function setTimeoutBuilder(job) {
+  return setTimeout(() => {
+    const data = job.data;
+    job.data = [];
+    //keep timeout reference to clear it after running jobHandle
+    const timeout = job.timeout;
+    job.timeout = undefined; //clear timeout reference that have been run
+    job.jobHandle
+      .apply(job, [data])
+      .catch((err) => {
+        console.log({ error: err.message, stack: err.stack });
+      })
+      .finally(() => {
+        //clear timeout after running jobHandle
+        clearTimeout(timeout);
+        timeout.unref();
+      });
+  }, job.interval);
+}
+
 class Job {
   constructor({
     interval = 1000,
@@ -8,8 +30,13 @@ class Job {
     jobHandle,
     name = `Job_${new Date().getTime()}`,
   } = {}) {
-    if (jobHandle === undefined || typeof jobHandle !== "function") {
-      throw new TypeError("jobHandle must be provided and must be a function");
+    if (
+      jobHandle === undefined ||
+      (typeof jobHandle !== "function" && !types.isAsyncFunction(jobHandle))
+    ) {
+      throw new TypeError(
+        "jobHandle must be provided and must be a function or async function",
+      );
     }
     if (!isNumber(interval)) {
       throw new TypeError("interval must be a number");
@@ -19,7 +46,11 @@ class Job {
     }
     this.interval = interval;
     this.max = max;
-    this.jobHandle = jobHandle;
+    this.timeout = undefined;
+    //make sure job.jobHandle is always a async function
+    this.jobHandle = types.isAsyncFunction(jobHandle)
+      ? jobHandle
+      : async (data) => jobHandle(data);
     this.name = name;
     this.data = [];
   }
@@ -29,22 +60,17 @@ class Job {
       //clear timeout if any
       if (this.timeout !== undefined) {
         clearTimeout(this.timeout);
+        this.timeout.unref();
         this.timeout = undefined;
       }
       //call jobHandle with all data and clear data
       const data = this.data;
       this.data = [];
-      this.jobHandle(data);
+      this.jobHandle(data).catch((err) => {
+        console.log({ error: err.message, stack: err.stack });
+      });
     } else if (this.timeout === undefined) {
-      const job = this;
-      this.timeout = setTimeout(() => {
-        const data = job.data;
-        job.data = [];
-        job.jobHandle.apply(job, [data]);
-        //clear timeout after running jobHandle
-        clearTimeout(this.timeout);
-        job.timeout = undefined;
-      }, this.interval);
+      this.timeout = setTimeoutBuilder(this);
     }
     this.data.push(value);
   }

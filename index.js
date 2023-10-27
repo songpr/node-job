5a856e2769a3f976753692de134a949dc306f466
Songpr <songpr@gmail.com>
2023-10-28 01:19:04 +0700

    check valid inteval and max

--- index.js
+++ index.js
@@ -41,8 +41,16 @@ class Job {
     if (!isNumber(interval)) {
       throw new TypeError("interval must be a number");
     }
+    if (interval < 1 || interval > 2147483647) {
+      throw new TypeError(
+        "interval must be greater than 0 and less than 2147483647",
+      );
+    }
     if (!isNumber(max)) {
       throw new TypeError("max must be a number");
+    }
+    if (max < 1 || max > 100000000) {
+      throw new TypeError("max must be greater than 0 and less than 100000000");
     }
     this.interval = interval;
     this.max = max;
@@ -56,23 +64,30 @@ class Job {
   }
 
   addData(value) {
+    //case data.length >= max
     if (this.data.length >= this.max) {
       //clear timeout if any
       if (this.timeout !== undefined) {
-        clearTimeout(this.timeout);
-        this.timeout.unref();
+        const timeout = this.timeout;
         this.timeout = undefined;
+        clearTimeout(timeout);
+        timeout.unref();
       }
       //call jobHandle with all data and clear data
       const data = this.data;
       this.data = [];
+      data.push(value); //add new value
       this.jobHandle(data).catch((err) => {
         console.log({ error: err.message, stack: err.stack });
       });
     } else if (this.timeout === undefined) {
+      //case data.length < max and timeout is not set
+      this.data.push(value);
       this.timeout = setTimeoutBuilder(this);
+    } else {
+      //case data.length < max and timeout is set
+      this.data.push(value);
     }
-    this.data.push(value);
   }
 }
 
